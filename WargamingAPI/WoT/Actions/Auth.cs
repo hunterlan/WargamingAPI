74345ccbe782d8020437e483ef99844dce90b83c
hunterlan <kotyshe@gmail.com>
2020-07-15 00:32:42 +0300

    Started working with auth

---
+++ WargamingAPI/WoT/Actions/Auth.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WargamingAPI.WoT.Actions
+{
+    public class Auth
+    {
+        public readonly string authLink;
+        public readonly List<string> typesInqury;
+
+        public Auth()
+        {
+            authLink = "https://api.worldoftanks.ru/wot/auth";
+            typesInqury = new List<string>() { "login/?", "prolongate/?", "logout/?" };
+        }
+        public string GetAccessToken(string application_id)
+        {
+            string token = "";
+            string finalUrlRequest = string.Concat(authLink, typesInqury[0],
+                "application_id=", application_id);
+
+            string response = Request.GetResponse(finalUrlRequest);
+            dynamic parsed = JsonConvert.DeserializeObject(response);
+            string status = parsed.status;
+
+            if(status == "ok")
+            {
+                token = parsed.access_token;
+            }
+
+            return token;
+        }
+    }
+}

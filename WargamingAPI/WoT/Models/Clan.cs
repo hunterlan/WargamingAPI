a09f048bf54f1d736250c153c8bd36b8fee7787d
hunterlan <kotyshe@gmail.com>
2020-07-19 14:59:27 +0300

    Added model Clan

---
+++ WargamingAPI/WoT/Models/Clan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WargamingAPI.WoT.Models
+{
+    class Clan
+    {
+        public int members_count { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+        public int created_at { get; set; }
+        public string tag { get; set; }
+        public int clan_id { get; set; }
+    }
+}

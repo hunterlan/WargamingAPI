4c39c5e015699f4c782827e71f6bbcaf91e4cc1d
hunterlan <kotyshe@gmail.com>
2020-07-19 15:32:40 +0300

    Added new models

---
+++ WargamingAPI/WoT/Models/Clans/ClanInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WargamingAPI.WoT.Models.Clans
+{
+    public class ClanInfo
+    {
+        public int leader_id { get; set; }
+        public string color { get; set; }
+        public int updated_at { get; set; }
+        public string tag { get; set; }
+        public int members_count { get; set; }
+        public string description_html { get; set; }
+        public bool accepts_join_requests { get; set; }
+        public string leader_name { get; set; }
+        public int clan_id { get; set; }
+        public int renamed_at { get; set; }
+        public string old_tag { get; set; }
+        public string description { get; set; }
+        public List<Member> members { get; set; }
+        public string old_name { get; set; }
+        public bool is_clan_disbanded { get; set; }
+        public string motto { get; set; }
+        public string name { get; set; }
+        public string creator_name { get; set; }
+        public int created_at { get; set; }
+        public int creator_id { get; set; }
+
+    }
+}
---
+++ WargamingAPI/WoT/Models/Clans/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WargamingAPI.WoT.Models.Clans
+{
+    public class Member
+    {
+        public string role { get; set; }
+        public string role_i18n { get; set; }
+        public int joined_at { get; set; }
+        public int account_id { get; set; }
+        public string account_name { get; set; }
+
+    }
+}

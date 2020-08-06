53f6b292c0ae99b120e82e314e77045ce9f7f00d
hunterlan <kotyshe@gmail.com>
2020-08-07 01:07:49 +0300

    Added new model

---
+++ WargamingAPI/WoT/Models/Clans/Rating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WargamingAPI.WoT.Models.Clans
+{
+    public class Rating
+    {
+        public int? rank_delta { get; set; }
+
+        public int? rank { get; set; }
+
+        public int value { get; set; }
+
+        public string name_rating { get; set; }
+    }
+}

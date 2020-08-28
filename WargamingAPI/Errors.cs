0b79879f69fb957ef83bd16395b083675b5d9868
hunterlan <kotyshe@gmail.com>
2020-08-29 02:48:06 +0300

    Developed new way of showing exception

---
+++ WargamingAPI/Errors.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace WargamingAPI
+{
+    [XmlType("error")]
+    public class Error
+    {
+        [XmlElement("name")]
+        public string TypeError { get; set; }
+        [XmlElement("description")]
+        public string Description { get; set; }
+    }
+    [XmlRoot("errors")]
+    public class Errors
+    {
+        [XmlElement("error")]
+        public List<Error> errors { get; set; }
+    }
+
+    public class XMLErrors
+    {
+        public Errors errors { get; set; }
+        public void ReadErrors()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Errors));
+
+            using (var reader = File.Open("ErrorString.xml", FileMode.Open))
+            {
+                errors = (Errors)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
---
+++ WargamingAPI/WoT/Exceptions/CauseException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WargamingAPI.WoT.Exceptions
+{
+    public class CauseException
+    {
+        public void Cause(string error)
+        {
+            XMLErrors init = new XMLErrors();
+            init.ReadErrors();
+            var listErrors = init.errors.errors;
+
+            for (int i = 0; i < listErrors.Capacity; i++)
+            {
+                if (string.Equals(error, listErrors[i].TypeError))
+                {
+                    throw new SearchException(listErrors[i].Description);
+                }
+            }
+
+            throw new Exception("Something went wrong");
+        }
+    }
+}

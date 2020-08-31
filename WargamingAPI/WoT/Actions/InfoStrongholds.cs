d13b8830403cc527fbf7895c280b6346c61c87fb
hunterlan <kotyshe@gmail.com>
2020-08-31 23:46:20 +0300

    Fixes #1: Implemented main functions for stronghold

--- WargamingAPI/WoT/Actions/InfoStrongholds.cs
+++ WargamingAPI/WoT/Actions/InfoStrongholds.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using WargamingAPI.WoT.Models.Clans;
+using WargamingAPI.WoT.Models.Clans.ClanReserves;
 using WargamingAPI.WoT.Models.Clans.Strongholds;
 
 namespace WargamingAPI.WoT.Actions
@@ -32,5 +33,22 @@ namespace WargamingAPI.WoT.Actions
 
             return stronghold;
         }
+
+        //Not sure about it. Can't check, because, I'm not in clan
+        public ClanReserve GetClanReserve(string application_id, string access_token)
+        {
+            ClanReserve reserve = new ClanReserve();
+
+            string finalUrlRequest = string.Concat(strongholdsLink, typesInqury[1],
+                "application_id=", application_id, "&access_token=", access_token);
+
+            string response = Request.GetResponse(finalUrlRequest);
+            var innerJson = JObject.Parse(response)["data"].ToString();
+            reserve = JsonConvert.DeserializeObject<ClanReserve>(innerJson);
+
+            return reserve;
+        }
+
+        //TO-DO: In future, add function, which activate reserves
     }
 }

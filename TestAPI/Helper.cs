2fc06b8ec7397855ba4ae4e067042cf1b1e505b2
Konstantin Sharykin <kotyshe@gmail.com>
2021-08-26 16:37:48 +0300

    Update tests and add new one
    I updated some tests. Function GetApiKey was taken out to
    separate class "Helper". New test check method GetClans.

---
+++ TestAPI/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace TestAPI
+{
+    public class Helper
+    {
+        public static string GetApiKey()
+        {
+            XmlDocument xDoc = new();
+            string apiKey = "";
+
+            string path = "";
+
+            for (int i = 0; i < 3; i++)
+            {
+                path = string.Concat(path, "..", Path.DirectorySeparatorChar);
+            }
+            path = string.Concat(path, "data.xml");
+            xDoc.Load(path);
+            XmlElement xRoot = xDoc.DocumentElement;
+            XmlNodeList nodes = xRoot.SelectNodes("/wot/apiKey");
+            foreach (XmlElement node in nodes)
+            {
+                apiKey = node.InnerText;
+            }
+
+            return apiKey;
+        }
+    }
+}
---
+++ TestAPI/WoT/InfoClansTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using WargamingAPI.Models.Clans;
+using WargamingAPI.WoT.Actions;
+
+namespace TestAPI.WoT
+{
+    public class InfoClansTest
+    {
+        private string _apiKey;
+        private InfoClans _infoClans;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _apiKey = Helper.GetApiKey();
+            _infoClans = new InfoClans();
+        }
+
+        [Test]
+        public void TestGetClans()
+        {
+            Clan expectedClan = new() { ClanId = 101221 };
+            var listClans = _infoClans.GetClans(_apiKey, "MERCY");
+            
+            if (listClans.Any(clan => clan.Equals(expectedClan)))
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail("Clan wasn't found");
+            }
+        }
+    }
+}
